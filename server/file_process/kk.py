e781635afda53d3a9ba1b0d5c0030fa5369c0d94
hyperingenious <skbmasale941@gmail.com>
2024-10-04 15:24:47 +0530

    whatever

--- server/file_process/kk.py
+++ server/file_process/kk.py
@@ -52,7 +52,7 @@ def get_conversational_chain():
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-    return chain
+    return chai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n
 
 
 
@@ -92,8 +92,6 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
-
-
 
 if __name__ == "__main__":
     main()
