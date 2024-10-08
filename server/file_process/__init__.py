19ca0e6bbb93e81fa0bcea6c57e91ddc8706bc1b
hyperingenious <skbmasale941@gmail.com>
2024-10-08 23:39:45 +0530

    file_process init file

--- server/file_process/__init__.py
+++ server/file_process/__init__.py
@@ -11,7 +11,6 @@ import PyPDF2
 
 project_id = '66dbf6d3002ec04e3664'
 bucket_id = '6700e87a00391724d5a6'
-pdf_path = '/home/hyper/Desktop/proj/specials/getsomequotes/server/uploads/infinity.pdf'
 
 def get_pdf_title(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -21,7 +20,7 @@ def get_pdf_title(pdf_path):
     print("PDF title retrieved.")
     return title
 
-def file_process():
+def file_process(pdf_path):
     pdf_upload_res = upload_pdf(pdf_path)
     print("PDF uploaded successfully.")
 
@@ -49,7 +48,6 @@ def file_process():
     chunks = chunk_pdf(pdf_path)
     print("PDF chunks created successfully.")
 
-    
     print('Started to upload chunks...')
     for chunk in chunks:
         chunk_data = {
@@ -62,7 +60,7 @@ def file_process():
     five_percent_random_chunk = retrive_5_percent_random_chunks(upload_book_entry_id)
     print("Retrieved 5% random chunks.")
 
-    filepath = f'{os.getcwd()}/selected_json/keshave.txt'
+    filepath = f'{os.getcwd()}/selected_json/{uuid.uuid4}.txt'
 
     with open(filepath, 'w', encoding='utf-8') as text_file:
         for document in five_percent_random_chunk.get("documents", []):
@@ -79,24 +77,3 @@ def file_process():
     print("Blog generation completed successfully.")
 
     print("It went successfully")
-
-file_process()
-
-
-# For Reference
-"""
-    {
-    '$id': '6701030024dfd3823a1f',
-    'bucketId': '6700e87a00391724d5a6', 
-    '$createdAt': '2024-10-05T09:12:32.696+00:00', 
-    '$updatedAt': '2024-10-05T09:12:32.696+00:00', 
-    '$permissions': [], 
-    'name': 'infinity.pdf', 
-    'signature': 'b687208e0644f423edf625c8e4b3af41', 
-    'mimeType': 'application/pdf', 
-    'sizeOriginal': 2182138, 
-    'chunksTotal': 1, 
-    'chunksUploaded': 1
-    }
-"""
-# https://cloud.appwrite.io/v1/storage/buckets/<bucket_id>/files/<file_id>/view?project=<project_id>&project=<project_id>&mode=admin
