77baa9b1be13148c607ac91c80262fcfbe303129
hyperingenious <skbmasale941@gmail.com>
2024-10-10 17:11:35 +0530

    Cleanup

--- server2/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    from .upload import upload
-
-    app.register_blueprint(upload, url_prefix='/')
-    return app
