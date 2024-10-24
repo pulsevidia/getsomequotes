a2f49eda994f420bc4d01151332d25e85b2883cc
hyperingenious <skbmasale941@gmail.com>
2024-10-24 18:41:29 +0530

    ADD: BookSekeleton Update

--- web/src/pages/Uploaded/BookListSkeleton.jsx
+++ web/src/pages/Uploaded/BookListSkeleton.jsx
@@ -2,7 +2,7 @@ import { Group, Skeleton } from '@mantine/core';
 
 function BookListSkeleton() {
   return <Group m={'md'} w={'100%'} gap={'xs'}>
-    {Array.from({ length: 10 }).map((_, index) => (
+    {Array.from({ length: 20 }).map((_, index) => (
       <Skeleton key={index} height={18} mt={6} width="100%" radius="xs" />
     ))}
   </Group>
