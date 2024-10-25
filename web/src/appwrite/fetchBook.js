7a733be237daf7e3e2c9e30e6132e52be57c34a6
hyperingenious <skbmasale941@gmail.com>
2024-10-25 19:35:13 +0530

    FIX: minor fix

--- web/src/appwrite/fetchBook.js
+++ web/src/appwrite/fetchBook.js
@@ -1,25 +1,34 @@
-import { Query } from "appwrite"
-import { databases } from "./appwrite"
+import { Query } from "appwrite";
+import { databases } from "./appwrite";
 
 async function fetchBook() {
   try {
-    const { documents } = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_BOOKS_COLLECTION_ID)
-    
-    for (let i = 0; i < documents.length; i++) {
+    const { documents } = await databases.listDocuments(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_BOOKS_COLLECTION_ID
+    );
 
+    for (let i = 0; i < documents.length; i++) {
       const current_document = documents[i];
 
-      const { documents: blogs } = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_BLOGS_COLLECTION_ID, [Query.equal("books", current_document['$id']), Query.select(['$id'])])
+      const { documents: blogs } = await databases.listDocuments(
+        import.meta.env.VITE_DATABASE_ID,
+        import.meta.env.VITE_BLOGS_COLLECTION_ID,
+        [Query.equal("books", current_document.$id), Query.select(["$id"])]
+      );
 
-      const { documents: quotes } = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_QUOTE_COLLECTION_ID, [Query.equal("books", current_document['$id']), Query.select(['$id'])])
+      const { documents: quotes } = await databases.listDocuments(
+        import.meta.env.VITE_DATABASE_ID,
+        import.meta.env.VITE_QUOTE_COLLECTION_ID,
+        [Query.equal("books", current_document.$id), Query.select(["$id"])]
+      );
 
-      documents[i] = { ...current_document, blogs, quotes }
+      documents[i] = { ...current_document, blogs, quotes };
     }
-    return documents 
+    return documents;
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
-
-export { fetchBook }
+export { fetchBook };
