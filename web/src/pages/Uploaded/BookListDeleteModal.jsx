02b2fc9cf7d41a79f9ad768b419fb919be0eb26b
hyperingenious <skbmasale941@gmail.com>
2024-10-25 19:38:35 +0530

    FIX: minor fix

--- web/src/pages/Uploaded/BookListDeleteModal.jsx
+++ web/src/pages/Uploaded/BookListDeleteModal.jsx
@@ -5,60 +5,89 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 function BookListDeleteModal({ isOpened, close, bookId }) {
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-    // status can be idle, pending, success, error
-    const { mutateAsync: deleteBook, status } = useMutation({
-        mutationFn: deleteBookAndDataCompletely,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["book"] })
-        }
-    })
+  // status can be idle, pending, success, error
+  const { mutateAsync: deleteBook, status } = useMutation({
+    mutationFn: deleteBookAndDataCompletely,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["book"] });
+    },
+  });
 
-    const handleDelete = async () => {
-        try {
-            await deleteBook(bookId)
-            close()
-            toast.success('Deleted Successfully', {
-
-                style: {
-                    backgroundColor: 'rgba(0, 0, 0, 1)',
-                    color: 'white'
-                }
-            })
-        } catch (error) {
-            close()
-            toast.error('Rate limit exceeded', {
-                style: {
-                    backgroundColor: 'rgba(0, 0, 0, 1)',
-                    color: 'white'
-                }
-            })
-            console.error(error)
-        }
+  const handleDelete = async () => {
+    try {
+      await deleteBook(bookId);
+      close();
+      toast.success("Deleted Successfully", {
+        style: {
+          backgroundColor: "rgba(0, 0, 0, 1)",
+          color: "white",
+        },
+      });
+    } catch (error) {
+      close();
+      toast.error("Rate limit exceeded", {
+        style: {
+          backgroundColor: "rgba(0, 0, 0, 1)",
+          color: "white",
+        },
+      });
+      console.error(error);
     }
+  };
 
-    return (
-        <Modal radius={'md'} styles={{
-            header: {
-                display: 'none'
-            }
-            , body: {
-                padding: '1.5rem',
-                paddingBottom: '1rem', paddingRight: '1.5rem', paddingLeft: '1.5rem',
-            }
-        }} centered opened={isOpened} onClose={close} title="Authentication">
-            <Group gap={'xs'} wrap="nowrap" >
-                <Warning size={42} weight="light" />
-                <Title order={4} fw={500}>Delete Content?</Title>
-            </Group>
-            <Divider my={'sm'} />
-            <Text fz={'sm'} color="dimmed">Are you sure you want to delete this content?</Text>
-            <Group gap={'sm'} justify="flex-end" mt="md" mb="xs">
-                <Button onClick={close} disabled={status == 'pending'} variant="transparent" color="rgba(0, 0, 0, 1)" size="sm">Cancel</Button>
-                <Button variant="light" loading={status == 'pending'} loaderProps={{ type: 'oval' }} onClick={handleDelete} color="rgba(0, 0, 0, 1)" size="sm">Delete</Button>
-            </Group>
-        </Modal>
-    )
+  return (
+    <Modal
+      radius={"xl"}
+      styles={{
+        header: {
+          display: "none",
+        },
+        body: {
+          padding: "1.5rem",
+          paddingBottom: "1rem",
+          paddingRight: "1.5rem",
+          paddingLeft: "1.5rem",
+        },
+      }}
+      centered
+      opened={isOpened}
+      onClose={close}
+      title="Authentication"
+    >
+      <Group gap={"xs"} wrap="nowrap">
+        <Warning size={42} weight="light" />
+        <Title order={4} fw={500}>
+          Delete Content?
+        </Title>
+      </Group>
+      <Divider my={"sm"} />
+      <Text fz={"sm"} color="dimmed">
+        Are you sure you want to delete this content?
+      </Text>
+      <Group gap={"sm"} justify="flex-end" mt="md" mb="xs">
+        <Button
+          onClick={close}
+          disabled={status == "pending"}
+          variant="transparent"
+          color="rgba(0, 0, 0, 1)"
+          size="sm"
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="light"
+          loading={status == "pending"}
+          loaderProps={{ type: "oval" }}
+          onClick={handleDelete}
+          color="rgba(0, 0, 0, 1)"
+          size="sm"
+        >
+          Delete
+        </Button>
+      </Group>
+    </Modal>
+  );
 }
 export default BookListDeleteModal;
