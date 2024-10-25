7b7fbd84673493592f609b6475af41bd70462386
hyperingenious <skbmasale941@gmail.com>
2024-10-25 17:19:26 +0530

    .vscode removed

--- web/src/appwrite/appwrite.js
+++ web/src/appwrite/appwrite.js
@@ -1,8 +1,9 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
 const client = new Client()
     .setEndpoint(import.meta.env.VITE_PROJECT_URL) // Your API Endpoint
     .setProject(import.meta.env.VITE_PROJECT_ID); // Your project ID
 
 export const databases = new Databases(client);
-export const databaseID = import.meta.env.VITE_DATABASE_ID 
+export const storage = new Storage(client);
+export const databaseID = import.meta.env.VITE_DATABASE_ID
