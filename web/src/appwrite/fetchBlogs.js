e5080e02458acc8762c967511cbda383fb2d2fa2
hyperingenious <skbmasale941@gmail.com>
2024-10-27 03:47:38 +0530

    FAKE COMMIT

--- web/src/appwrite/fetchBlogs.js
+++ web/src/appwrite/fetchBlogs.js
@@ -2,9 +2,12 @@ import { databases } from "./appwrite";
 
 export async function fetchBlogs() {
   try {
-    const { documents } = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID, import.meta.env.VITE_BLOGS_COLLECTION_ID);
+    const { documents } = await databases.listDocuments(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_BLOGS_COLLECTION_ID
+    );
     return documents;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
