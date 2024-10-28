56bc2d7ce0e3e6ef24388443da9a429584755a08
hyperingenious <skbmasale941@gmail.com>
2024-10-28 23:19:12 +0530

    Limit increaed

--- web/src/appwrite/fetchBlogs.js
+++ web/src/appwrite/fetchBlogs.js
@@ -1,10 +1,13 @@
+import { Query } from "appwrite";
 import { databases } from "./appwrite";
 
 export async function fetchBlogs() {
   try {
     const { documents } = await databases.listDocuments(
       import.meta.env.VITE_DATABASE_ID,
+
       import.meta.env.VITE_BLOGS_COLLECTION_ID
+      , [Query.limit(2833)]
     );
     return documents;
   } catch (error) {
