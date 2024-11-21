1d053ccd3abacddf4a199c1fbcb8f8ab4042d157
hyperingenious <skbmasale941@gmail.com>
2024-11-22 02:59:49 +0530

    REMOVE: Removed Vite Code(can access in vite_older_branch)

--- web/src/helpers/helper.js
+++
@@ -1,8 +0,0 @@
-function extractFirstLine(markdown){
-	const start = markdown.indexOf("##");
-	if(start == -1) return null;
-	const end = markdown.indexOf('\n', start);
-	if(end===-1) return null;
-	const what = [markdown.slice(start +2, end).trim()];
-	return what}
-export {extractFirstLine}
