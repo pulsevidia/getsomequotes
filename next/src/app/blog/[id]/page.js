e01e5131d9ca47e21d7ce7f5d06f5d0057a106c7
hyperingenious <skbmasale941@gmail.com>
2024-11-23 01:47:14 +0530

    ADD/REMOVE: SmallBlogCard & TitleCompoenet separated

--- next/src/app/blog/[id]/page.js
+++ next/src/app/blog/[id]/page.js
@@ -2,14 +2,11 @@
 import {
   Center,
   Loader,
-  Title,
   Text,
   Image,
   Badge,
   Stack,
   Code,
-  Card,
-  Avatar,
   Group,
   ScrollArea,
   useComputedColorScheme,
@@ -17,20 +14,14 @@ import {
 } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { getBlogAndSuggestedBlogs } from "../../../appwrite/getBlogById";
-import Link from "next/link";
 import { useParams } from "next/navigation";
 import Markdown from "markdown-to-jsx";
 import QuoteCard from "../../components/QuoteCard";
 import { useResizeObserver } from "@mantine/hooks";
 import { cardShadows } from "../../utils/shadows";
-import { DM_Sans, Afacad_Flux, Spectral } from "next/font/google";
-
-const dm_sans = DM_Sans({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-});
+import { Afacad_Flux, Spectral } from "next/font/google";
+import SmallBlogCard from "@/app/components/SmallBlogCard";
+import TitleComponent from "@/app/components/TitleComponent";
 
 const afacad_flux = Afacad_Flux({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -48,18 +39,6 @@ const spectral = Spectral({
 
 // List of images
 const allImages = Array.from({ length: 24 }, (_, i) => `${i + 1}${i >= 12 ? ".webp" : ".jpg"}`);
-
-// Custom components
-const TitleComponent = ({ children, order = 1, style = {} }) => {
-  const theme = useMantineTheme();
-  const colorScheme = useComputedColorScheme();
-  const color = colorScheme === "dark" ? "#f1beb5" : theme.colors.gray[9];
-  return (
-    <Title c={color} style={{ ...style }} className={spectral.className} fw={500} order={order}>
-      {children}
-    </Title>
-  );
-};
 
 const TextMarkdown = ({ children }) => (
   <Text py="xs" ta="left" size="lg" className={spectral.className}>
@@ -93,51 +72,8 @@ const MarkdownToCustom = ({ markdown }) => (
   </Markdown>
 );
 
-const BlogCard = ({ blog, colorScheme, theme, bookImage, author }) => (
-  <Link href={`/blog/${blog.$id}`} style={{ textDecoration: "none" }}>
-    <Card
-      shadow={cardShadows.xs}
-      bg={colorScheme === "dark" ? "rgb(19, 27, 46)" : theme.colors.gray[2]}
-      miw={300}
-      mih={137}
-      p="md"
-      radius="lg"
-    >
-      <Title
-        lineClamp={2}
-        mih={43}
-        fw={500}
-        order={4}
-        c={colorScheme === "dark" ? "#f1beb5" : theme.colors.gray[9]}
-        className={afacad_flux.className}
-        style={{ lineHeight: 1.2 }}
-      >
-        {blog.blogTitle || "Surprise Blog It Has No Title"}
-      </Title>
-      <Text
-        c={colorScheme === "dark" ? "rgba(182, 141, 133, 0.46)" : theme.colors.gray[6]}
-        fw={500}
-        mt={5}
-        size="sm"
-        lineClamp={2}
-        className={dm_sans.className}
-        style={{ lineHeight: 1.1 }}
-      >
-        {blog.blogContent}
-      </Text>
-      <Group gap={4} align="center" mt="xs">
-        <Avatar size="xs" src={bookImage} alt="Author" />
-        <Text c={colorScheme === "dark" ? "rgb(182, 141, 133)" : theme.colors.gray[6]} size="sm">
-          {author}
-        </Text>
-      </Group>
-    </Card>
-  </Link>
-);
-
 function ReadBlog() {
   const [ref, rect] = useResizeObserver();
-  console.log(rect);
 
   const theme = useMantineTheme();
   const { id } = useParams();
@@ -223,11 +159,9 @@ function ReadBlog() {
       <ScrollArea w={rect.width} h={200}>
         <Group gap="xs" mt="xs" wrap="nowrap">
           {allBlogsWithBookId.map((blog) => (
-            <BlogCard
+            <SmallBlogCard
               key={blog.$id}
               blog={blog}
-              colorScheme={colorScheme}
-              theme={theme}
               bookImage={blogData.books.book_image}
               author={blogData.books.author}
             />
---
+++ next/src/app/components/SmallBlogCard.jsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import { Avatar, Card, Group, Text, Title, useComputedColorScheme, useMantineTheme } from "@mantine/core";
+import { cardShadows } from "../utils/shadows";
+import { Afacad_Flux, DM_Sans } from "next/font/google";
+
+const dm_sans = DM_Sans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const afacad_flux = Afacad_Flux({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+function SmallBlogCard({ blog, key, bookImage, author }) {
+  const theme = useMantineTheme();
+  const colorScheme = useComputedColorScheme();
+
+  return (
+    <Link key={key} href={`/blog/${blog.$id}`} style={{ textDecoration: "none" }}>
+      <Card
+        shadow={cardShadows.xs}
+        bg={colorScheme === "dark" ? "rgb(19, 27, 46)" : theme.colors.gray[2]}
+        miw={300}
+        mih={137}
+        p="md"
+        radius="lg"
+      >
+        <Title
+          lineClamp={2}
+          mih={43}
+          fw={500}
+          order={4}
+          c={colorScheme === "dark" ? "#f1beb5" : theme.colors.gray[9]}
+          className={afacad_flux.className}
+          style={{ lineHeight: 1.2 }}
+        >
+          {blog.blogTitle || "Surprise Blog It Has No Title"}
+        </Title>
+        <Text
+          c={colorScheme === "dark" ? "rgba(182, 141, 133, 0.46)" : theme.colors.gray[6]}
+          fw={500}
+          mt={5}
+          size="sm"
+          lineClamp={2}
+          className={dm_sans.className}
+          style={{ lineHeight: 1.1 }}
+        >
+          {blog.blogContent}
+        </Text>
+        <Group gap={4} align="center" mt="xs">
+          <Avatar size="xs" src={bookImage} alt="Author" />
+          <Text c={colorScheme === "dark" ? "rgb(182, 141, 133)" : theme.colors.gray[6]} size="sm">
+            {author}
+          </Text>
+        </Group>
+      </Card>
+    </Link>
+  );
+}
+export default SmallBlogCard;
