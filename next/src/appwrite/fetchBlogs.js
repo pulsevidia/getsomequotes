e5af1b9c13f688819901b3798f5c5c84e88538b9
hyperingenious <skbmasale941@gmail.com>
2024-11-25 06:00:25 +0530

    ADD: blog fetching from id Array

--- next/src/appwrite/fetchBlogs.js
+++ next/src/appwrite/fetchBlogs.js
@@ -1,7 +1,22 @@
 import { Query } from "appwrite";
 import { databases } from "./appwrite";
+import { getBlogById } from "./getBlogById";
 
-export async function fetchBlogs(id) {
+async function fetchBlogsWithIdArray({ idsArray, user_id }) {
+  try {
+    const blogs = [];
+    for (const id of idsArray) {
+      const result = await getBlogById(id, user_id);
+      blogs.push(result);
+    }
+    return blogs;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function fetchBlogs(id) {
   try {
     const { documents } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_DATABASE_ID,
@@ -9,7 +24,7 @@ export async function fetchBlogs(id) {
       [Query.limit(2000), Query.equal("user_id", [id])]
     );
     const refined_documents = [];
-    
+
     documents.forEach((doc) => {
       doc.books ? refined_documents.push(doc) : null;
     });
@@ -19,3 +34,4 @@ export async function fetchBlogs(id) {
     console.error(error);
   }
 }
+export { fetchBlogsWithIdArray, fetchBlogs };
