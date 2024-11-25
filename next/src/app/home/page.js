00d1f3123c748956d5c75006d5ffb123ced86ecb
hyperingenious <skbmasale941@gmail.com>
2024-11-25 06:02:44 +0530

    Optimze: Skeleton is spearated

--- next/src/app/home/page.js
+++ next/src/app/home/page.js
@@ -1,45 +1,17 @@
 "use client";
-import {
-  Group,
-  Skeleton,
-  Stack,
-  useComputedColorScheme,
-  useMantineTheme,
-} from "@mantine/core";
+import { Stack, useComputedColorScheme } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { fetchBlogs } from "@/appwrite/fetchBlogs";
 import { useUser } from "@clerk/nextjs";
 import BlogCard from "../components/BlogCard";
+import BlogSkeleton from "../components/BlogSkeleton";
 
 export default function Home() {
   const {
     user: { id },
   } = useUser();
-  const theme = useMantineTheme();
+
   const colorScheme = useComputedColorScheme();
-  function BlogSkeleton() {
-    return (
-      <>
-        {Array.from({ length: 10 }).map((_, i) => (
-          <Group
-            key={i}
-            wrap="nowrap"
-            bg={
-              colorScheme === "dark" ? "rgb(19, 27, 46)" : theme.colors.gray[0]
-            }
-            p={"lg"}
-          >
-            <Skeleton height={80} width={80} />
-            <Stack gap={"xs"}>
-              <Skeleton mb={"xs"} height={13} radius={"xl"} width={100} />
-              <Skeleton height={13} radius={"xl"} width={200} />
-              <Skeleton height={13} radius={"xl"} width={200} />
-            </Stack>
-          </Group>
-        ))}
-      </>
-    );
-  }
 
   const {
     data: blogsData,
@@ -49,11 +21,11 @@ export default function Home() {
     queryFn: () => fetchBlogs(id),
     queryKey: ["blogs"],
   });
+
   return (
     <Stack pb={"100"}>
-      {isBlogsLoading && <BlogSkeleton />}
-      {isBlogsSuccess &&
-        blogsData.map((blog) => <BlogCard blog={blog} key={blog.$id} />)}
+      {isBlogsLoading && <BlogSkeleton colorScheme={colorScheme} instances={10} />}
+      {isBlogsSuccess && blogsData.map((blog) => <BlogCard blog={blog} key={blog.$id} />)}
     </Stack>
   );
 }
