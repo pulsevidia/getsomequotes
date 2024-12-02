231ae3e2d7fced8694b4fb627264b57f0df1402d
hyperingenious <skbmasale941@gmail.com>
2024-12-02 05:56:05 +0530

    OPTIMIZE/ORGNIZE

--- src/app/components/layout/AppShellLayout.jsx
+++ src/app/components/layout/AppShellLayout.jsx
@@ -13,6 +13,7 @@ import NavigationRoutes from "./NavRoutes";
 
 import { dark_theme } from "@/app/config/theme";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { memo, useMemo } from "react";
 
 function AppShellLayout({ children }) {
   const mantineTheme = useMantineTheme();
@@ -23,6 +24,61 @@ function AppShellLayout({ children }) {
   const colorScheme = useComputedColorScheme();
   const [isNavbarOpen, { toggle: toggleNavbar }] = useDisclosure();
   const { user } = useUser();
+
+  const AppShellHeader = memo(() => (
+    <AppShell.Header bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
+      <Group justify="space-between" h="100%" px="md">
+        {isTabletScreen && <Avatar src={user?.imageUrl} onClick={toggleNavbar} alt={user?.fullName} size={32} />}
+        <BrandLogo />
+        {isTabletScreen && <ThemeSwitcher />}
+        {!isTabletScreen && <FeedbackButton />}
+      </Group>
+    </AppShell.Header>
+  ));
+
+  const AppShellNavigation = useMemo(
+    () => (
+      <AppShell.Navbar p="md" bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
+        <Stack gap={0} h="100%" justify="space-between">
+          <Stack gap={0}>
+            <UserProfileCard
+              colorScheme={colorScheme}
+              fullName={user?.fullName}
+              emailAddress={user?.emailAddresses[0]?.emailAddress}
+              imageUrl={user?.imageUrl}
+              color={colorScheme === "dark" ? "rgb(19, 27, 46)" : mantineTheme.colors.gray[0]}
+            />
+            <NavigationRoutes toggle={toggleNavbar} colorScheme={colorScheme} />
+          </Stack>
+          <Group mb={"md"} justify="space-between" gap={0}>
+            <SignOutButton>
+              <Group
+                gap="xs"
+                align="center"
+                p="sm"
+                justify="center"
+                style={{
+                  cursor: "pointer",
+                }}
+              >
+                <Text size="sm" c={colorScheme === "dark" ? dark_theme.main_text_color : "red"} fontWeight={500}>
+                  Sign out
+                </Text>
+                <SignOut
+                  weight="bold"
+                  color={colorScheme === "dark" ? dark_theme.main_text_color : "#fa5252"}
+                  size={16}
+                />
+              </Group>
+            </SignOutButton>
+            {!isSmallScreen && <ThemeSwitcher />}
+            {isSmallScreen && <FeedbackButton />}
+          </Group>
+        </Stack>
+      </AppShell.Navbar>
+    ),
+    [colorScheme, isSmallScreen, user]
+  );
 
   return (
     <>
@@ -41,58 +97,8 @@ function AppShellLayout({ children }) {
           }}
         >
           <Toaster position="bottom-center" reverseOrder={false} />
-
-          {/* Header */}
-          <AppShell.Header bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
-            <Group justify="space-between" h="100%" px="md">
-              {isTabletScreen && <Avatar src={user?.imageUrl} onClick={toggleNavbar} alt={user?.fullName} size={32} />}
-              <BrandLogo />
-              {isTabletScreen && <ThemeSwitcher />}
-              {!isTabletScreen && <FeedbackButton />}
-            </Group>
-          </AppShell.Header>
-
-          {/* Navbar */}
-          <AppShell.Navbar p="md" bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
-            <Stack gap={0} h="100%" justify="space-between">
-              <Stack gap={0}>
-                <UserProfileCard
-                  colorScheme={colorScheme}
-                  fullName={user?.fullName}
-                  emailAddress={user?.emailAddresses[0]?.emailAddress}
-                  imageUrl={user?.imageUrl}
-                  color={colorScheme === "dark" ? "rgb(19, 27, 46)" : mantineTheme.colors.gray[0]}
-                />
-                <NavigationRoutes toggle={toggleNavbar} colorScheme={colorScheme} />
-              </Stack>
-              <Group mb={"md"} justify="space-between" gap={0}>
-                <SignOutButton>
-                  <Group
-                    gap="xs"
-                    align="center"
-                    p="sm"
-                    justify="center"
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Text size="sm" c={colorScheme === "dark" ? dark_theme.main_text_color : "red"} fontWeight={500}>
-                      Sign out
-                    </Text>
-                    <SignOut
-                      weight="bold"
-                      color={colorScheme === "dark" ? dark_theme.main_text_color : "#fa5252"}
-                      size={16}
-                    />
-                  </Group>
-                </SignOutButton>
-                {!isSmallScreen && <ThemeSwitcher />}
-                {isSmallScreen && <FeedbackButton />}
-              </Group>
-            </Stack>
-          </AppShell.Navbar>
-
-          {/* Main Content */}
+          <AppShellHeader />
+          {AppShellNavigation}
           <AppShell.Main style={{ paddingInline: isCompactScreen ? 0 : undefined }}>{children}</AppShell.Main>
 
           {/* Bottom Navigation for Small Screens */}
