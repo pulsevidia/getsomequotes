2775432a7b19c16d51a88767a4cc081ca8b19824
hyperingenious <skbmasale941@gmail.com>
2024-12-02 07:26:05 +0530

    ADD: Variables added

--- src/appwrite/appwrite.js
+++ src/appwrite/appwrite.js
@@ -6,4 +6,13 @@ const client = new Client()
 
 export const databases = new Databases(client);
 export const storage = new Storage(client);
+
+export const projectID = process.env.NEXT_PUBLIC_PROJECT_ID;
 export const databaseID = process.env.NEXT_PUBLIC_DATABASE_ID;
+export const bucketID = process.env.NEXT_PUBLIC_BUCKET_ID;
+export const blogsCollectionID = process.env.NEXT_PUBLIC_BLOGS_COLLECTION_ID;
+export const booksCollectionID = process.env.NEXT_PUBLIC_BOOKS_COLLECTION_ID;
+export const chunksCollectionID = process.env.NEXT_PUBLIC_CHUNKS_COLLECTION_ID;
+export const appwriteAPIKey = process.env.NEXT_PUBLIC_APPWRITE_API_KEY;
+export const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+export const clerkSecretKey = process.env.CLERK_SECRET_KEY;
