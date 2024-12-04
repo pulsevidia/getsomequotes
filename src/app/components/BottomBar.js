6b4910b6367311539f1fc594b215a1613d26dccd
hyperingenious <skbmasale941@gmail.com>
2024-12-04 12:25:11 +0530

    OPTIMIZE:  Bottombar optimized 🚀

--- src/app/components/BottomBar.js
+++ src/app/components/BottomBar.js
@@ -1,6 +1,6 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./BottomBar.module.css";
-import { House, FileArrowUp, UploadSimple, Books } from "@phosphor-icons/react";
+import { House, FileArrowUp, Books } from "@phosphor-icons/react";
 import { usePathname, useRouter } from "next/navigation";
 import { dark_theme } from "../config/theme";
 import { ScrollArea, Stack, useComputedColorScheme, useMantineTheme } from "@mantine/core";
@@ -10,6 +10,8 @@ import LoadingSkeleton from "./bottom-bar/Skeleton";
 import BookCards from "./bottom-bar/BookCards";
 import { useQuery } from "@tanstack/react-query";
 import { useUser } from "@clerk/clerk-react";
+import ActiveBookCard from "./bottom-bar/ActiveBookCard";
+import Link from "next/link";
 
 const BottomBar = () => {
   const [isExpanded, toggleExpanded] = useToggle();
@@ -33,29 +35,13 @@ const BottomBar = () => {
   });
 
   const iconColor = useMemo(
-    () => (colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[9]),
+    () => (colorScheme == "dark" ? dark_theme.main_text_color : theme.colors.gray[9]),
     [colorScheme, theme.colors.gray]
   );
 
-  const navigationOptions = useMemo(
-    () => [
-      { name: "/", icon: <House size={80} /> },
-      { name: "/upload_book", icon: <Books size={80} /> },
-      { name: "/uploaded", icon: <FileArrowUp size={80} /> },
-    ],
-    []
-  );
-
-  const handleNavigation = (name) => {
-    if (name === "/upload_book") {
-      toggleExpanded();
-    } else {
-      router.push(name);
-      if (isExpanded) toggleExpanded();
-    }
-  };
-
   const [activeBook, setActiveBook] = useState(null);
+  const [currentRoute, setCurrentRoute] = useState(null);
+  useEffect(() => setCurrentRoute(pathname), []);
   return (
     <div
       style={{
@@ -70,14 +56,14 @@ const BottomBar = () => {
     >
       <div
         style={{
-          background: colorScheme === "dark" ? dark_theme.nav_link_dark_color : "white",
+          background: colorScheme == "dark" ? dark_theme.nav_link_dark_color : "white",
           padding: "8px",
         }}
         className={styles.nav_parent}
       >
         <ScrollArea
           ref={ref}
-          bg={colorScheme === "dark" ? "rgb(11, 9, 28)" : theme.colors.gray[3]}
+          bg={colorScheme == "dark" ? "rgb(11, 9, 28)" : theme.colors.gray[0]}
           scrollbarSize={2}
           h={isExpanded ? 300 : 0}
           style={{ transition: "0.3s ", borderRadius: "30px" }}
@@ -86,24 +72,62 @@ const BottomBar = () => {
           <Stack gap="xs" m="xs">
             {activeBook && <ActiveBookCard {...activeBook} colorScheme={colorScheme} />}
             {isSuccess &&
-              books.map((data, i) => <BookCards key={i} {...data} colorScheme={colorScheme} toggle={toggleExpanded} />)}
+              books.map(
+                (data, i) =>
+                  data.$id !== activeBook && (
+                    <BookCards
+                      setActiveBook={setActiveBook}
+                      key={i}
+                      {...data}
+                      colorScheme={colorScheme}
+                      toggle={toggleExpanded}
+                    />
+                  )
+              )}
             {isLoading && <LoadingSkeleton colorScheme={colorScheme} />}
           </Stack>
         </ScrollArea>
         <nav className={styles.nav}>
-          {navigationOptions.map((item) => (
-            <a
-              key={item.name}
-              className={`${styles.link} ${pathname === item.name ? styles.active : ""}`}
-              onClick={(e) => {
-                e.preventDefault();
-                handleNavigation(item.name);
-              }}
-              style={{ "--hover-bg": `${iconColor}20`, "--hover-c": iconColor }}
-            >
-              {item.icon}
-            </a>
-          ))}
+          <Link
+            href={"/"}
+            className={styles.link}
+            onClick={(e) => {
+              setCurrentRoute("/");
+              if (isExpanded) toggleExpanded();
+            }}
+            style={{
+              "--hover-bg": `${iconColor}20`,
+              "--hover-c": iconColor,
+              background: currentRoute == "/" && "var(--hover-bg)",
+              color: currentRoute == "/" && "var(--hover-c)",
+            }}
+          >
+            <House size={80} weight={currentRoute == "/" ? "fill" : "regular"} />
+          </Link>
+          <div
+            className={styles.link}
+            onClick={() => {
+              toggleExpanded();
+            }}
+          >
+            <Books size={80} />
+          </div>
+          <Link
+            href={"/uploaded"}
+            className={`${styles.link}`}
+            onClick={() => {
+              setCurrentRoute("/uploaded");
+              if (isExpanded) toggleExpanded();
+            }}
+            style={{
+              "--hover-bg": `${iconColor}20`,
+              "--hover-c": iconColor,
+              background: currentRoute == "/uploaded" && "var(--hover-bg)",
+              color: currentRoute == "/uploaded" && "var(--hover-c)",
+            }}
+          >
+            <FileArrowUp weight={currentRoute == "/uploaded" ? "fill" : "regular"} size={80} />
+          </Link>
         </nav>
       </div>
     </div>
