eb9025e362e9561025aa54f73612d739bc9d847f
hyperingenious <skbmasale941@gmail.com>
2024-12-04 12:18:22 +0530

    REFINE: Blog skeleton refied

--- src/app/components/BlogSkeleton.jsx
+++ src/app/components/BlogSkeleton.jsx
@@ -1,21 +1,54 @@
-import { Group, Skeleton, Stack, useMantineTheme } from "@mantine/core";
-import { memo } from "react";
+import { Card, Group, Skeleton, Stack, useComputedColorScheme, useMantineTheme } from "@mantine/core";
+import { dark_theme } from "../config/theme";
+import { useMediaQuery } from "@mantine/hooks";
 
-function BlogSkeleton({ colorScheme, instances }) {
+function BlogSkeleton({ instances = 1 }) {
   const theme = useMantineTheme();
+  const colorScheme = useComputedColorScheme();
+  const cardBackground = colorScheme === "dark" ? dark_theme.nav_link_dark_color : theme.colors.gray[0];
+  const isSmallScreen = useMediaQuery("(max-width: 480px)");
+
   return (
     <>
       {Array.from({ length: instances }).map((_, i) => (
-        <Group key={i} wrap="nowrap" bg={colorScheme === "dark" ? undefined : theme.colors.gray[0]} p={"lg"}>
-          <Skeleton height={80} width={80} />
-          <Stack gap={"xs"}>
-            <Skeleton mb={"xs"} height={13} radius={"xl"} width={100} />
-            <Skeleton height={13} radius={"xl"} width={200} />
-            <Skeleton height={13} radius={"xl"} width={200} />
-          </Stack>
-        </Group>
+        <Card
+          key={i}
+          shadow="sm"
+          maw={600}
+          style={{ cursor: "pointer" }}
+          mx="xs"
+          radius="md"
+          padding={0}
+          bg={cardBackground}
+        >
+          <Group gap="xs" align="flex-start" wrap="nowrap">
+            <Skeleton
+              miw={isSmallScreen ? 120 : 140}
+              mih={isSmallScreen ? 167 : 150}
+              width={isSmallScreen ? 120 : 140}
+              height={isSmallScreen ? 167 : 150}
+              style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 6px" }}
+            />
+            <Stack pr="sm" py="sm" gap="xs">
+              <Group mb="xs" gap="xs" align="flex-start">
+                <Skeleton height={13} radius="xl" width={70} />
+                <Skeleton height={13} radius="xl" width={40} />
+                <Skeleton height={13} radius="xl" width={50} />
+              </Group>
+              {/* <Skeleton height={15} radius="sm" width={200} />
+              <Skeleton height={15} radius="sm" width={200} />
+              <Skeleton height={10} radius="sm" width={220} />
+              <Skeleton height={10} radius="sm" width={220} /> */}
+              <Skeleton height={15} radius="sm" width={400} />
+              <Skeleton height={15} radius="sm" width={200} />
+              <Skeleton height={10} radius="sm" width={420} />
+              <Skeleton height={10} radius="sm" width={220} />
+            </Stack>
+          </Group>
+        </Card>
       ))}
     </>
   );
 }
-export default memo(BlogSkeleton);
+
+export default BlogSkeleton;
