119f39493cc2ce5a159318b9e6e18d2d247b1b46
hyperingenious <skbmasale941@gmail.com>
2024-12-31 10:53:41 +0530

    REMOVE: user_id removed

--- src/appwrite/fetchBook.js
+++ src/appwrite/fetchBook.js
@@ -2,26 +2,25 @@ import { Query } from "appwrite";
 import { databases } from "./appwrite";
 /**
  * Fetches data about the book then fetches associated content number and return book
- * @param {string} id
+ * @param {string} user_id
  * @returns {Array}
  */
-async function fetchBook(id) {
+async function fetchBook(user_id) {
   try {
     const { documents } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_DATABASE_ID,
       process.env.NEXT_PUBLIC_BOOKS_COLLECTION_ID,
-      [Query.equal("user_id", [id])]
+      [Query.equal("user_id", [user_id])]
     );
 
     for (let i = 0; i < documents.length; i++) {
       const current_document = documents[i];
-
       const { documents: blogs } = await databases.listDocuments(
         process.env.NEXT_PUBLIC_DATABASE_ID,
         process.env.NEXT_PUBLIC_BLOGS_COLLECTION_ID,
         [
           Query.equal("books", current_document.$id),
-          Query.equal("user_id", [id]),
+          Query.equal("user_id", [user_id]),
           Query.select(["$id"]),
         ]
       );
