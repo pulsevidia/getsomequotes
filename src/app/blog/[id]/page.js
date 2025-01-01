3388d3549878dc1c0eb6535fd88f0c7da575762d
hyperingenious <skbmasale941@gmail.com>
2025-01-01 11:38:26 +0530

    IDK

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,16 +1,6 @@
 "use client";
 import {
-  Center,
-  Loader,
-  Text,
-  Image,
-  Badge,
-  Stack,
-  Code,
-  Group,
-  ScrollArea,
-  useComputedColorScheme,
-  useMantineTheme,
+  Center, Loader, Text, Image, Badge, Stack, Code, Group, ScrollArea, useComputedColorScheme, useMantineTheme,
 } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { getBlogAndSuggestedBlogs } from "../../../appwrite/getBlogById";
@@ -24,6 +14,7 @@ import TitleComponent from "@/app/components/TitleComponent";
 import { useUser } from "@clerk/clerk-react";
 import { afacad_flux, spectral } from "@/app/font";
 import { dark_theme } from "@/app/config/theme";
+import { checkIfAtLeastOneBookIsThere } from "@/appwrite/checkIfAtLeastOneBookIsThere";
 
 const TextMarkdown = ({ children }) => (
   <Text fw={400} py="xs" ta="left" size="lg" className={spectral.className}>
@@ -90,49 +81,21 @@ function ReadBlog() {
   }
   const { blogData, allBlogsWithBookId } = data;
   return (
-    <Stack
-      ref={ref}
-      w={"100%"}
-      miw={300}
-      align="start"
-      maw={800}
-      px="md"
-      mx="auto"
-      gap="lg"
-      mb={isSmallScreen ? 100 : 0}
+    <Stack ref={ref} w={"100%"} miw={300} align="start" maw={800} px="md" mx="auto" gap="lg" mb={isSmallScreen ? 100 : 0}
     >
       {/* Blog Image */}
-      <Image
-        w="100%"
-        miw={300}
-        maw={800}
-        src={blogData?.blog_image || `/images_4_blogs/1.jpg`}
-        alt="JUST a shthetic image"
-        style={{ boxShadow: cardShadows.xs }}
-        radius="md"
-        mih={300}
-        mah={300}
-      />
+      <Image w="100%" miw={300} maw={800} src={blogData?.blog_image || `/images_4_blogs/1.jpg`} alt="JUST a shthetic image" style={{ boxShadow: cardShadows.xs }} radius="md" mih={300} mah={300} />
 
       <Stack gap={0}>
         {/* Book Badge */}
-        <Badge
-          color={colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[6]}
-          mt="md"
-          variant="light"
-          size="lg"
-          className={afacad_flux.className}
-          style={{ boxShadow: cardShadows.xs }}
+        <Badge color={colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[6]} mt="md" variant="light" size="lg" className={afacad_flux.className} style={{ boxShadow: cardShadows.xs }}
         >
           {blogData.books.book_name}
         </Badge>
 
         {/* Author Name */}
         <Text
-          c={colorScheme === "dark" ? "#febeb5" : theme.colors.gray[6]}
-          ta="left"
-          size="md"
-          className={afacad_flux.className}
+          c={colorScheme === "dark" ? "#febeb5" : theme.colors.gray[6]} ta="left" size="md" className={afacad_flux.className}
         >
           —{blogData.books?.author || "Unknown"}
         </Text>
