9d2c837c6cc73b5a211e4b84d3a816e3de33e140
hyperingenious <skbmasale941@gmail.com>
2025-02-06 00:32:47 +0530

    ADD & CHANGE: security checks & initiate transaction changed to POST

--- src/app/components/SubscriptionModal.jsx
+++ src/app/components/SubscriptionModal.jsx
@@ -1,5 +1,5 @@
 import { useMediaQuery } from '@mantine/hooks';
-import { Modal, Button, useMantineTheme, useComputedColorScheme, Title, Stack, Text, Card, Group, List, Center, ScrollArea } from '@mantine/core';
+import { Modal, Button, useMantineTheme, useComputedColorScheme, Title, Stack, Text, Card, Group, List, Center, ScrollArea, LoadingOverlay } from '@mantine/core';
 import { dark_theme } from '../config/theme';
 import { cardShadows } from '../utils/shadows';
 import { Check, Crown, CrownCross, CrownSimple } from '@phosphor-icons/react';
@@ -9,74 +9,96 @@ import toast from 'react-hot-toast';
 import { useState } from 'react';
 import { useAuth } from '@clerk/nextjs';
 
-function SubscriptionModal({ opened, close }) {
-    const theme = useMantineTheme();
-    const colorScheme = useComputedColorScheme();
-    const isBigEnoughScreen = useMediaQuery('(min-width: 1150px)');
-    /*
-        async function initiateSubscription({ subscription_type, getToken, setIsLoading }) {
-            try {
-                if (subscription_type === 'free') return;
-                setIsLoading(true)
-                const token = await getToken({ template: "supabase_2" })
-                const url = `${process.env.NEXT_PUBLIC_NODE_SERVER_URL}initiate-transaction?subscription_type=${subscription_type}`
-    
-                const response = await fetch(url, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    },
-                })
-    
-                if (!response.ok) {
-                    const error = await response.json();
-                    throw error
-                }
-    
-                const responseData = await response.json()
-                setIsLoading(false)
-                window.location.href = responseData.payment_link;
-                return
-            } catch (error) {
-                console.error(error)
-                toast.error(error.message)
+function SubscriptionCard({ setIsAnythingLoading, subscription_type, title, price, icon, bg, features, button, isBigEnoughScreen }) {
+    const colorScheme = useComputedColorScheme()
+    const [isLoading, setIsLoading] = useState(false)
+    const { getToken } = useAuth()
+
+    async function initiateSubscription({ subscription_type, getToken, setIsLoading }) {
+        try {
+            if (subscription_type === 'free') {
+                setIsLoading(false);
+                setIsAnythingLoading(false);
+                return;
             }
+
+            setIsLoading(true)
+            setIsAnythingLoading(true)
+            const token = await getToken({ template: "supabase_2" })
+            const url = `${process.env.NEXT_PUBLIC_NODE_SERVER_URL}initiate-transaction`
+
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"  // Add this line
+                },
+                body: JSON.stringify({ subscription_type })
+            });
+
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw error
+            }
+
+            const responseData = await response.json()
+            const payment_link = responseData.payment_link
+            if (payment_link.startsWith('https://rzp.io/rzp/') && payment_link.length === 26) {
+                window.location.href = responseData.payment_link;
+            }
+
+            setIsLoading(false)
+
+            return
+        } catch (error) {
+            toast.error(error.message)
+            console.error(error)
+            setIsLoading(false)
+            setIsAnythingLoading(false)
+
         }
-    */
+    }
+
 
     return (
-        <Modal size={isBigEnoughScreen ? '70em' : '21em'} opened={opened} radius={"xl"}
-            styles={{
-                header: { display: "none" },
-                body: {
-                    padding: "0.5rem",
-                    paddingBottom: "1rem",
-                    background: colorScheme === "dark" ? dark_theme.app_bg_dark_color : "white",
-                    paddingRight: isBigEnoughScreen ? '1.5rem' : "0.5rem",
-                    paddingLeft: isBigEnoughScreen ? '1.5rem' : "0.5rem",
-                },
-            }}
-            centered bg={theme.colors.gray[0]} onClose={close}>
-            <Stack p={'sm'} gap={0}>
-                <Title ml={'xs'} fw={500} className={poppins.className} mt={4} order={2}>Choose a plan</Title>
-                <Text ml={'xs'} className={dm_sans.className} c={'dimmed'}>Select the offer that best suits your needs</Text>
-                {isBigEnoughScreen ? (
-                    <Group wrap='nowrap' justify='center'>
-                        {plans.map(plan => (
-                            <SubscriptionCard key={plan.title} {...plan} isBigEnoughScreen={isBigEnoughScreen} />
-                        ))}
+        <Card miw={isBigEnoughScreen ? 300 : 295} ml={isBigEnoughScreen ? 'sm' : 0} mr={!isBigEnoughScreen && 'xs'} mt={'lg'} p={isBigEnoughScreen ? 'xl' : 'sm'} radius={'lg'} bg={bg}>
+            <Group gap={'lg'}>
+                <Card w={60} h={60} p={0} bg={'white'} shadow={cardShadows.xs} radius={'lg'}>
+                    <Center h={'60'}>
+                        {icon}
+                    </Center>
+                </Card>
+                <Stack gap={0}>
+                    <Title fw={500} className={afacad_flux.className} order={3}>{title}</Title>
+                    <Group gap={0} m={0}>
+                        <Text className={afacad_flux.className} fw={600} size='xl'>{price}</Text>
                     </Group>
-                ) : (
-                    <Carousel style={{ cursor: 'grab' }} align={'center'} controlsOffset="0" withControls>
-                        {plans.map(plan => (
-                            <SubscriptionCard subscription_type={plan.subscription_type} key={plan.title} {...plan} isBigEnoughScreen={isBigEnoughScreen} />
-                        ))}
-                    </Carousel>
-                )}
-            </Stack>
-        </Modal>
+                </Stack>
+            </Group>
+            <List mt={'md'} size="sm" className={dm_sans.className} center icon={<Check size={18} color={colorScheme === 'dark' ? 'gray' : 'black'} weight="bold" />}>
+                <ScrollArea w={300} h={250} scrollbars="y">
+                    {features.map((feature, index) => (
+                        <List.Item key={index} pb={'xs'}>{feature}</List.Item>
+                    ))}
+                </ScrollArea>
+            </List>
+            <Button
+                loading={isLoading}
+                loaderProps={{ type: 'dots' }}
+                onClick={async () => await initiateSubscription({ getToken, subscription_type, setIsLoading })}
+                {...button}
+                mt={'md'}
+                size='lg'
+                fz={'md'}
+                color={'black'}
+                style={button.variant === 'filled' ? { boxShadow: cardShadows.xs } : {}} className={dm_sans.className} fw={900} radius={'xl'}>
+                {button.text}
+            </Button>
+        </Card>
     );
 }
+
 const plans = [
     {
         title: "Limited",
@@ -142,77 +164,45 @@ const plans = [
     },
 ];
 
-function SubscriptionCard({ subscription_type, title, price, icon, bg, features, button, isBigEnoughScreen }) {
-    const colorScheme = useComputedColorScheme()
-    const [isLoading, setIsLoading] = useState(false)
-    const { getToken } = useAuth()
 
-    async function initiateSubscription({ subscription_type, getToken, setIsLoading }) {
-        try {
-            if (subscription_type === 'free') return;
-            setIsLoading(true)
-            const token = await getToken({ template: "supabase_2" })
-            const url = `${process.env.NEXT_PUBLIC_NODE_SERVER_URL}initiate-transaction?subscription_type=${subscription_type}`
-
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${token}`
-                },
-            })
-
-            if (!response.ok) {
-                const error = await response.json();
-                throw error
-            }
-
-            const responseData = await response.json()
-            window.location.href = responseData.payment_link;
-            setIsLoading(false)
-            return
-        } catch (error) {
-            toast.error(error.message)
-            console.error(error)
-            setIsLoading(false)
-        }
-    }
-
+function SubscriptionModal({ opened, close }) {
+    const theme = useMantineTheme();
+    const colorScheme = useComputedColorScheme();
+    const isBigEnoughScreen = useMediaQuery('(min-width: 1150px)');
+    const [isAnythingLoading, setIsAnythingLoading] = useState(false)
 
     return (
-        <Card miw={isBigEnoughScreen ? 300 : 295} ml={isBigEnoughScreen ? 'sm' : 0} mr={!isBigEnoughScreen && 'xs'} mt={'lg'} p={isBigEnoughScreen ? 'xl' : 'sm'} radius={'lg'} bg={bg}>
-            <Group gap={'lg'}>
-                <Card w={60} h={60} p={0} bg={'white'} shadow={cardShadows.xs} radius={'lg'}>
-                    <Center h={'60'}>
-                        {icon}
-                    </Center>
-                </Card>
-                <Stack gap={0}>
-                    <Title fw={500} className={afacad_flux.className} order={3}>{title}</Title>
-                    <Group gap={0} m={0}>
-                        <Text className={afacad_flux.className} fw={600} size='xl'>{price}</Text>
+        <Modal size={isBigEnoughScreen ? '70em' : '21em'} opened={opened} radius={"xl"}
+            styles={{
+                header: { display: "none" },
+                body: {
+                    padding: "0.5rem",
+                    paddingBottom: "1rem",
+                    background: colorScheme === "dark" ? dark_theme.app_bg_dark_color : "white",
+                    paddingRight: isBigEnoughScreen ? '1.5rem' : "0.5rem",
+                    paddingLeft: isBigEnoughScreen ? '1.5rem' : "0.5rem",
+                },
+            }}
+            centered bg={theme.colors.gray[0]} onClose={close}>
+            <LoadingOverlay visible={isAnythingLoading} zIndex={1000} overlayProps={{ c: 'dark', radius: "sm", blur: 2 }} />
+            <Stack p={'sm'} gap={0}>
+                <Title ml={'xs'} fw={500} className={poppins.className} mt={4} order={2}>Choose a plan</Title>
+                <Text ml={'xs'} className={dm_sans.className} c={'dimmed'}>Select the offer that best suits your needs</Text>
+                {isBigEnoughScreen ? (
+                    <Group wrap='nowrap' justify='center'>
+                        {plans.map(plan => (
+                            <SubscriptionCard subscription_type={plan.subscription_type} setIsAnythingLoading={setIsAnythingLoading} key={plan.title} {...plan} isBigEnoughScreen={isBigEnoughScreen} />
+                        ))}
                     </Group>
-                </Stack>
-            </Group>
-            <List mt={'md'} size="sm" className={dm_sans.className} center icon={<Check size={18} color={colorScheme === 'dark' ? 'gray' : 'black'} weight="bold" />}>
-                <ScrollArea w={300} h={250} scrollbars="y">
-                    {features.map((feature, index) => (
-                        <List.Item key={index} pb={'xs'}>{feature}</List.Item>
-                    ))}
-                </ScrollArea>
-            </List>
-            <Button
-                loading={isLoading}
-                loaderProps={{ type: 'dots' }}
-                onClick={async () => await initiateSubscription({ getToken, subscription_type, setIsLoading })}
-                {...button}
-                mt={'md'}
-                size='lg'
-                fz={'md'}
-                color={'black'}
-                style={button.variant === 'filled' ? { boxShadow: cardShadows.xs } : {}} className={dm_sans.className} fw={900} radius={'xl'}>
-                {button.text}
-            </Button>
-        </Card>
+                ) : (
+                    <Carousel style={{ cursor: 'grab' }} align={'center'} controlsOffset="0" withControls>
+                        {plans.map(plan => (
+                            <SubscriptionCard setIsAnythingLoading={setIsAnythingLoading} subscription_type={plan.subscription_type} key={plan.title} {...plan} isBigEnoughScreen={isBigEnoughScreen} />
+                        ))}
+                    </Carousel>
+                )}
+            </Stack>
+        </Modal>
     );
 }
 
