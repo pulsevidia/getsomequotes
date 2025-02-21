16df93b45ac49ac48691c76a27c9cf1c68ec7365
hyperingenious <skbmasale941@gmail.com>
2025-02-22 00:59:18 +0530

    ADD: Subscription card logic modified for new subscription

--- src/app/components/layout/SubscriptionCard.jsx
+++ src/app/components/layout/SubscriptionCard.jsx
@@ -4,7 +4,7 @@ import { cardShadows } from "@/app/utils/shadows";
 import { getSubscription } from "@/appwrite/get/getSubscription";
 import { useAuth } from "@clerk/nextjs";
 import { Button, Card, Center, Group, Loader, Stack, Text } from "@mantine/core";
-import { Crown, CrownCross, CrownSimple } from "@phosphor-icons/react";
+import { Crown, CrownCross } from "@phosphor-icons/react";
 import { useQuery } from "@tanstack/react-query";
 
 function SubscriptionCard({ open, colorScheme }) {
@@ -21,7 +21,7 @@ function SubscriptionCard({ open, colorScheme }) {
     }
 
     return (
-        <Card bg={colorScheme === 'dark' ? dark_theme.nav_link_dark_color: ''} shadow={cardShadows.xs} radius="md" py="xs" px="sm" mb="md">
+        <Card bg={colorScheme === 'dark' ? dark_theme.nav_link_dark_color : ''} shadow={cardShadows.xs} radius="md" py="xs" px="sm" mb="md">
             {
                 isLoading &&
                 <Loader size={'xs'} color={colorScheme === 'dark' ? dark_theme.main_text_color : 'dark'} type="dots" mx={'auto'} />
@@ -34,18 +34,13 @@ function SubscriptionCard({ open, colorScheme }) {
                             <Center h={'60'}>
 
                                 {
-                                    data.isActiveSubscription && data.subscription_type == 'reader' &&
+                                    !data.isActiveSubscription &&
                                     <Crown size={18} color="#9c9c9c" weight="fill" />
                                 }
                                 {
-                                    data.isActiveSubscription && data.subscription_type == 'avid_reader' &&
+                                    data.isActiveSubscription && data.subscription_type == 'reader' &&
                                     <CrownCross size={18} color="#edbd0c" weight="fill" />
                                 }
-                                {
-                                    !data.isActiveSubscription &&
-                                    <CrownSimple size={22} color="#a25915" weight="fill" />
-                                }
-
                             </Center>
                         </Card>
                         <Stack gap={0}>
@@ -54,14 +49,18 @@ function SubscriptionCard({ open, colorScheme }) {
                                 {data.isActiveSubscription && data.subscription_type == 'reader' && "Reader"}
                             </Text>
                             <Group w={'100%'} justify="flex-start" wrap="nowrap">
+                                <Text size="xs" c="dimmed" fw={600}>
+                                    {!data.isActiveSubscription && `Books: ${data.freeBookCount}/5`}
+                                </Text>
+ 
                                 {!data.isActiveSubscription &&
                                     <Button
-                                        leftSection={<Crown weight="duotone" size={22} />}
-                                        size='sm' gradient={{ from: 'violet', to: 'grape', deg: 90 }} variant="gradient" className={dm_sans.className} radius={'xl'} onClick={open}>Get Plan</Button>}
-                                <Text size="xs" c="dimmed">
+                                        leftSection={<Crown weight="duotone" size={18} />}
+                                        size='xs' gradient={{ from: 'violet', to: 'grape', deg: 90 }} variant="gradient" className={dm_sans.className} radius={'xl'} onClick={open}>Get Plan</Button>}
+                                <Text size="xs" c="dimmed" fw={500}>
                                     {data.isActiveSubscription && `blogs: ${data.quota.blogs_generated}/${data.quota.allocated_blog_quota}`}
                                 </Text>
-                                {
+                               {
                                     data.isActiveSubscription &&
                                     <Text fw={500} size="xs" c="dimmed">
                                         ends: {formatDate(data.end_date)}
